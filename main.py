878904ceb4341616b2221cdfb8e5d4c50b0ff9bb
jorchris <jorchris@gmail.com>
2026-09-23 16:33:15 -0300

    Add network device data

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-nome = "jorchris"
-print(nome)
+device = {
+    "hostname": "SW01",
+    "ip": "10.10.10.1",
+    "vendor": "Cisco",
+    "model": "Catalyst 9300"
+}
+
+print(device["hostname"])
+print(device["ip"])
+print(device["vendor"])
